e84d7fce9f6dc7450fbdf596e4191f704f737b6d
Nicolas LaGamma <nicolaslagamma@gmail.com>
2023-08-28 00:18:45 -0400

    name fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # build image from Dockerfile
-podman build -t portable_box2 .
+podman build -t portable_box .
 # start container from image
-podman run -d -it --name test_container_a2 localhost/portable_box2
+podman run -d -it --name test_container_a localhost/portable_box
 
 # attach as non-root user
 #podman exec -it test_container_a bash
