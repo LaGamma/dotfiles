df082e5eacf161f0f932fc2ac9e7c022e1ff5461
Nicolas LaGamma <nicolaslagamma@gmail.com>
2023-10-06 12:54:49 -0400

    add tmux config and x11 in docker setup instructions

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ podman run -d -it --name test_container_a localhost/portable_box
 #podman exec -it test_container_a bash
 # attach as root user
 #podman exec -u 0 -it test_container_a bash
+#
+# run and mount X11 socket for local display forwarding
+#podman run -dit -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw --net=host --name my_box localhost/portable_box
+# enable X11 forwarding - (may need to do this with `xauth add <host>` inside the container)
+#podman cp .Xauthority test_container_a:/home/underpriv/.Xauthority
